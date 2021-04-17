b61447663bcdc1f7b93224c5cef1620e63ea16b3
David Chen <David.Y.Chen@ibm.com>
2021-04-17 15:59:47 -0700

    Gaussian / Binomial project added

---
+++ software-engineering-fundamentals-oop/3a_python_package/distributions/__init__.py
@@ -0,0 +1,1 @@
+from .Gaussian import Gaussian
---
+++ software-engineering-fundamentals-oop/3a_python_package/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name = "distributions",
+    version = "0.1",
+    descriprion = "Gaussian and other common distributions",
+    packages = ["distributions"],
+    zip_safe = False
+)
